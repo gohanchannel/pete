8400f4b802cff01b559323afcc21fd0c50299fa8
gohanchannel <32711634+gohanchannel@users.noreply.github.com>
2017-10-17 23:10:18 +0900

    Delete AreaJoin.java

--- src/main/java/com/github/gohanchannel/pete/AreaJoin.java
+++
@@ -1,9 +0,0 @@
-package com.github.gohanchannel.pete;
-
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class AreaJoin extends JavaPlugin {
-	public  static 
-}
